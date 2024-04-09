e21990fb03f30a40bf011809ddf537b0c4afe828
Tumi Thaki <tumiethaki01@gmail.com>
2024-04-09 08:09:20 -0700

    0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+    This module contains the function number_of_subscribers it queries the
+    Redddit API and returns the number of subscribers for a given subreddit
+'''
+import requests
+from sys import argv
+
+
+def number_of_subscribers(subreddit):
+    '''
+        returns the number of subscribers for a given subreddit
+    '''
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/about.json'
+                       .format(subreddit), headers=user).json()
+    try:
+        return url.get('data').get('subscribers')
+    except Exception:
+        return 0
+
+
+if __name__ == "__main__":
+    number_of_subscribers(argv[1])
